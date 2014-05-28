2fc19c64da74c968e16dcc4dc99731cb64e4845b
Live session user <ubuntu@ubuntu.(none)>
2014-05-28 09:55:36 +0000

    mas cercaaakafpmiafkla

---
+++ nomames.c
@@ -0,0 +1,61 @@
+#include<stdio.h> //For standard things
+#include<stdlib.h>    //malloc
+#include<string.h>    //memset
+#include<netinet/ip_icmp.h>   //Provides declarations for icmp header
+#include<netinet/udp.h>   //Provides declarations for udp header
+#include<netinet/tcp.h>   //Provides declarations for tcp header
+#include<netinet/ip.h>    //Provides declarations for ip header
+#include<netinet/if_ether.h> 
+#include<sys/socket.h>
+#include<arpa/inet.h>
+ 
+void ProcessPacket(unsigned char* , int);
+void print_ip_header(unsigned char* , int);
+void print_tcp_packet(unsigned char* , int);
+void print_udp_packet(unsigned char * , int);
+void print_icmp_packet(unsigned char* , int);
+void PrintData (unsigned char* , int);
+ 
+int sock_raw;
+FILE *logfile;
+int tcp=0,udp=0,icmp=0,others=0,igmp=0,total=0,i,j;
+struct sockaddr_in source,dest;
+ 
+int main()
+{
+    int saddr_size , data_size;
+    struct sockaddr saddr;
+    struct in_addr in;
+     
+    unsigned char *buffer = (unsigned char *)malloc(65536); //Its Big!
+     
+    logfile=fopen("log.txt","w");
+    if(logfile==NULL) printf("Unable to create file.");
+    printf("Starting...\n");
+    //Create a raw socket that shall sniff
+    sock_raw = socket(AF_INET , SOCK_RAW , 1); //NO FUNCIONA CON HTONS POR QUEEEE
+    if(sock_raw < 0)
+    {
+        printf("Socket Error\n");
+        return 1;
+    }
+    while(1)
+    {
+        saddr_size = sizeof saddr;
+        //Receive a packet
+        data_size = recvfrom(sock_raw , buffer , 65536 , 0 , &saddr , &saddr_size);
+        if(data_size <0 )
+        {
+            printf("Recvfrom error , failed to get packets\n");
+            return 1;
+        }
+        //Now process the packet
+        
+        printf("\n>D\n");
+        printf("\nip origen: %02X %02X %02X %02X \n", buffer[12], buffer[13], buffer[14], buffer[15]);
+    }
+    close(sock_raw);
+    printf("Finished");
+    return 0;
+}
+ 
